911ec34df2ea5118fc15fb94d3f0db2abc284826
liz <luppieliz@gmail.com>
2022-02-26 17:49:31 +0800

    FE employer changes

--- frontend/src/Commons/Components/ProgressSteps/ProgressSteps.js
+++ frontend/src/Commons/Components/ProgressSteps/ProgressSteps.js
@@ -1,105 +1,50 @@
 import { React, useState } from "react";
-import { Steps, Tabs, Popover, Button, Modal } from "antd";
+import { Steps } from "antd";
 
-import { Row, Col, ListGroup } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import "./style.css";
 
-import test from "./test.json";
-import test2 from "./test2.json";
+function ProgressSteps(prop) {
+  const { name, steps, number } = prop;
 
-function ProgressSteps() {
-  const [step, setStep] = useState(0);
+  const [step] = steps;
 
   const { Step } = Steps;
   const [leftStep, setLeftStep] = useState(0);
   const onClickLeft = (curr) => {
     setLeftStep(curr);
   };
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  const content = (
-    <div>
-      <p>Name : jerry</p>
-      <p>job applied : SOFTWARE DEV INTERN</p>
-      <p>Stage : 1</p>
-      <Button onClick={showModal}>applicant resume</Button>
-      <Modal
-        title="Basic Modal"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
-    </div>
-  );
-
+  console.log(steps);
   return (
-    <>
-      <Row>
-        <Col>
-          {test2.map((data) => (
-            <>
-              <div className="ProgressSteps__leftColumn">
-                <ListGroup>
-                  <Popover content={content} title="Applicant name">
-                    <ListGroup.Item className="ProgressSteps__leftItem">
-                      {data.name}
-                    </ListGroup.Item>
-                  </Popover>
-                </ListGroup>
-              </div>
-            </>
-          ))}
-        </Col>
-        <Col xs={10}>
-          {test.map((step, key) => (
-            <>
-              <div className="ProgressSteps__paddings">
-                <h2 className="ProgressSteps__heading"> applicant 1 </h2>
-                <Steps
-                  progressDot
-                  current={leftStep}
-                  onChange={onClickLeft}
-                  direction="horizontal"
-                >
-                  <Step
-                    title={step.title}
-                    description={step.desc}
-                    subtitle={step.subtitle}
-                  />
-                  <Step
-                    title={step.title2}
-                    description={step.desc2}
-                    subtitle={step.subtitle2}
-                  />
-                  <Step
-                    title={step.title3}
-                    description={step.desc3}
-                    subtitle={step.subtitle3}
-                  />
-                </Steps>
-              </div>
-            </>
-          ))}
-        </Col>
-      </Row>
-    </>
+    <Col xs={10}>
+      <div className="ProgressSteps__paddings">
+        <h2 className="ProgressSteps__heading">
+          {number}. {name}
+        </h2>
+        <Steps
+          progressDot
+          current={leftStep}
+          onChange={onClickLeft}
+          direction="horizontal"
+        >
+          <Step
+            title={step.title}
+            description={step.desc}
+            subtitle={step.subtitle}
+          />
+          <Step
+            title={step.title2}
+            description={step.desc2}
+            subtitle={step.subtitle2}
+          />
+          <Step
+            title={step.title3}
+            description={step.desc3}
+            subtitle={step.subtitle3}
+          />
+        </Steps>
+      </div>
+    </Col>
   );
 }
 
--- frontend/src/Components/EmployerUI/Components/EmployerDashboard/index.js
+++ frontend/src/Components/EmployerUI/Components/EmployerDashboard/index.js
@@ -1,18 +1,38 @@
 import { React, useState } from "react";
 import { Tabs } from "antd";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
 import ProgressSteps from "../../../../Commons/Components/ProgressSteps/ProgressSteps";
 import EditableTable from "../../../../Commons/Components/EditableTable/EditableTable";
+import LeftList from "../../../../Commons/Components/ProgressSteps/LeftList";
+
+import test from "./Data/test.json";
 
 function EmployerDashboard() {
   const { TabPane } = Tabs;
+  const [nameClicked, setNameClicked] = useState("");
+  console.log(nameClicked);
 
   return (
     <Container>
       <Tabs defaultActiveKey="1" centered>
         <TabPane tab="Intern Applicants" key="1">
-          <ProgressSteps />
+          <Row>
+            <Col>
+              <LeftList name={(app_name) => setNameClicked(app_name)} />
+            </Col>
+            <Col xs={10}>
+              {test.map((data, index) => (
+                <ProgressSteps
+                  key={index}
+                  number={data.no}
+                  name={data.Applicant_Name}
+                  job={data.job_applied}
+                  steps={data.steps}
+                />
+              ))}
+            </Col>
+          </Row>
         </TabPane>
         <TabPane tab="Manage Job Listings" key="2">
           <EditableTable />
